798a531b8069ae58c71c2ce20704c0cfa94dc9e6
jainy xavier <xavierjainy15@gmail.com>
2022-06-20 12:28:18 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// var btnColor = document.querySelector('.color')
+// var resultado = document.querySelector('p')
+
+// document.addEventListener('scroll', function(){
+//     console.log('scroll')
+//     btnColor.classList.toggle('ativo')
+
+//     var numero1 = parseInt(document.querySelector('#numero1').value)
+//     var numero2 = parseInt(document.querySelector('#numero2').value)
+
+//     resultado.innerHTML = soma(numero1, numero2)
+//     console.log(resultado)
+
+//     btnColor.style = 'background-color: green;'
+// })
+
+// function soma(num1, num2){
+//     return num1 + num2
+// }
+
+// var infoForm = document.querySelectorAll('.info-form').value
+var btnEnviar = document.querySelector('#btn-enviar')
+console.log(btnEnviar)
