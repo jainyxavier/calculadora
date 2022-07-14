147fb9710619da6b98438de3bffad2fad5424916
Jainy Xavier <xavierjainy15@gmail.com>
2022-07-14 11:16:39 -0300

    feature: calculadora interface e funcionalidades

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,92 @@
-// var btnColor = document.querySelector('.color')
-// var resultado = document.querySelector('p')
+var btnsNum = document.querySelectorAll('.num')
+// var btnSoma = document.querySelector('#soma')
+// var btnSubtracao = document.querySelector('#subtracao')
+// var btnMultiplicacao = document.querySelector('#multiplicacao')
+// var btnDivisao = document.querySelector('#divisao')
+var btnOperation = document.querySelectorAll('.operator')
+var p = document.querySelector('p')
+var btnIgual = document.querySelector('#igual')
+var btnClear = document.querySelector('#btn-clear')
+var num = ''
+var num1 = ''
+var num2 = ''
 
-// document.addEventListener('scroll', function(){
-//     console.log('scroll')
-//     btnColor.classList.toggle('ativo')
+btnsNum.forEach(btn => {
+    btn.addEventListener('click', function(){
+        num = num + btn.textContent
+        p.innerHTML = num
+        
+        if(num1 !== ''){
+            num2 = num
+            p.innerHTML = num1 + ' + ' + num2
+        }
+    })
+})
 
-//     var numero1 = parseInt(document.querySelector('#numero1').value)
-//     var numero2 = parseInt(document.querySelector('#numero2').value)
+btnOperation.forEach(operation => {
+    operation.addEventListener('click', function(){
+        num1 = num
+        p.innerHTML = num1 + ' + '
+    
+        if(num1 !== '' ){
+            num = ''
+        }
+    
+        if(num2 !== ''){
+            num = ''
+        }
+    })
+})
 
-//     resultado.innerHTML = soma(numero1, numero2)
-//     console.log(resultado)
+btnIgual.addEventListener('click', function(){
+    btnOperation.forEach(operation => {
+        if(operation.textContent == '+' && num1 !== '' && num2 !== ''){
+            somar(num1, num2)
+            num1 = ''
+            num2 = ''
+            num = ''
+        } else if(operation.textContent == '-' && num1 !== '' && num2 !== ''){
+            subtracao(num1, num2)
+            num1 = ''
+            num2 = ''
+            num = ''
+        } else if(operation.textContent == '*' && num1 !== '' && num2 !== ''){
+            multiplicacao(num1, num2)
+            num1 = ''
+            num2 = ''
+            num = ''
+        } else if(operation.textContent == '/' && num1 !== '' && num2 !== ''){
+            divisao(num1, num2)
+            num1 = ''
+            num2 = ''
+            num = ''
+        }
+    })
+})
 
-//     btnColor.style = 'background-color: green;'
-// })
+function somar(num1, num2){
+    var resultado = parseInt(num1) + parseInt(num2)
+    p.innerHTML = resultado
+}
 
-// function soma(num1, num2){
-//     return num1 + num2
-// }
+function subtracao(num1, num2){
+    var resultado = parseInt(num1.value) - parseInt(num2.value)
+    p.innerHTML = resultado
+}
 
-// var infoForm = document.querySelectorAll('.info-form').value
-// var btnEnviar = document.querySelector('#btn-enviar')
-// console.log(btnEnviar)
+function multiplicacao(num1, num2){
+    var resultado = parseInt(num1.value) * parseInt(num2.value)
+    p.innerHTML = resultado
+}
 
-var numero1 = parseInt(document.querySelector('#numero1').value)
-console.log('clicou')
+function divisao(num1, num2){
+    var resultado = parseInt(num1.value) / parseInt(num2.value)
+    p.innerHTML = resultado
+}
 
-
-
-// var numero2 = parseInt(document.querySelector('#numero2').value)
-// var resultado = parseInt(document.querySelector('p').value)
-
+btnClear.addEventListener('click', function(){
+    p.innerHTML = ''
+    num1 = ''
+    num2 = ''
+    num = ''
+})
